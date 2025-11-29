68f07ba3763e0226c5214f22df4fde509ec265bb
Aijazshaikrx21259 <rx21259@umbc.edu>
2025-11-29 23:58:25 +0530

    chore: add code formatting configuration (Prettier, EditorConfig)

---
+++ lib/utils/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Utility functions export barrel
+ */
+
+export * from "./date-helpers";
+export * from "./format";
+export * from "./validation";
+
+// Re-export cn from utils for convenience
+export { cn } from "@/lib/utils";
